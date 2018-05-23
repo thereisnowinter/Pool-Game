8103b8dcf96c4fd2e6348c52d1acce7a8c5769dc
thereisnowinter <505975669@qq.com>
2018-05-23 21:41:59 +0800

    too tired to fix a bug

--- Ball.js
+++ Ball.js
@@ -2,12 +2,18 @@ const BALL_ORIGIN = new Vector2 (25, 25)
 
 function Ball (position) {
   this.position = position
+  this.velocity = new Vector2 ()
 }
 
-Ball.prototype.update = function () {
-
+Ball.prototype.update = function (delta) {
+  this.position.addTo (this.velocity)
+  // this.velocity = this.velocity.mult (0.98)
 }
 
 Ball.prototype.draw = function () {
   Canvas.drawImage (sprites.whiteBall, this.position, BALL_ORIGIN)
 }
+
+Ball.prototype.shoot = function (power, rotation) {
+  this.velocity = new Vector2 (Math.cos (rotation), Math.sin (rotation))
+} 
--- Stick.js
+++ Stick.js
@@ -1,18 +1,28 @@
 const STICK_ORIGIN = new Vector2 (970, 11)
+const STICK_SHOOT_ORIGIN = new Vector2 (950, 11)
 
-function Stick (position) {
+function Stick (position, onShoot) {
   this.position = position
   this.rotation = 0
+  this.origin = STICK_ORIGIN.copy ()
+  this.power = 0
+  this.onShoot = onShoot
 }
 
 Stick.prototype.update = function () {
+  
+  if (Mouse.left.down) {
+    this.increasePower()
+  } else if (this.power > 0) {
+    this.shoot ()
+  }
   this.updateRotation()
  
 }
 
 
 Stick.prototype.draw = function () {
-  Canvas.drawImage(sprites.stick, this.position, STICK_ORIGIN, this.rotation)
+  Canvas.drawImage(sprites.stick, this.position, this.origin, this.rotation)
 }
 
 Stick.prototype.updateRotation = function () {
@@ -20,6 +30,17 @@ Stick.prototype.updateRotation = function () {
   let adjacent = Mouse.position.x - this.position.x
 
   this.rotation = Math.atan2(opposite, adjacent)
+}
+
+Stick.prototype.increasePower = function () {
+  this.power += 100
+  this.origin.x += 5
+}
+
+Stick.prototype.shoot = function () {
+  this.onShoot (this.power, this.rotation)
+  this.power = 0
+  this.origin = STICK_SHOOT_ORIGIN.copy ()
 }
 
 
